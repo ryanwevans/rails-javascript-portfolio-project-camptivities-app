7c9828490eafcd58a856a9c61c2ed87ebc7d2183
ryanwevans <ryanwevans@icloud.com>
2019-04-25 17:35:51 -0700

    fix Camp.prototype.formatShow to load DOM with camp.activities

--- app/assets/javascripts/camp.js
+++ app/assets/javascripts/camp.js
@@ -19,92 +19,84 @@ const bindClickHandlers = () => {
     })
   })
 
-    $(document).on('click', ".show_link", function(event) {
-      event.preventDefault()
-      $('#app_container').html('')
-      let id = $(this).attr('data-id')
-      history.pushState(null, null, `camps/${id}`);
-      fetch(`/camps/${id}.json`)
-      .then(response => response.json())
-      .then(camp => {
-        let newCamp = new Camp(camp)
-        let campHtml = newCamp.formatShow()
-        $('#app_container').append(campHtml)
-      })
-    })
-
-
-    $('#new_camp_form').on("submit", function(event) {
-      event.preventDefault()
-      $('#app_container').html('')
-
-      const values = $(this).serialize()
-
-      $.post("/camps", values).done(function(data) {
-        const newCamp = new Camp(data)
-        const htmlToAdd = newCamp.formatShow()
-        $('#app_container').append(htmlToAdd)
-      })
-    })
-  }
-
-// for the activities show link in a Camp view
-  $(document).on('click', ".show_activities_link", function(event) {
+  $(document).on('click', ".show_link", function(event) {
     event.preventDefault()
     $('#app_container').html('')
     let id = $(this).attr('data-id')
-    history.pushState(null, null, `camps/${id}/activities`);
-    fetch(`/camps/${id}/activities.json`)
+    history.pushState(null, null, `camps/${id}`);
+    fetch(`/camps/${id}.json`)
     .then(response => response.json())
-    .then(activities => {
-      $('#app_container').html('')
-      activities.forEach(activity => {
-        let newActivity = new Activity(activity)
-        let activityHtml = newActivity.formatIndex()
-        $('#app_container').append(activityHtml)
-      })
+    .then(camp => {
+      let newCamp = new Camp(camp)
+      let campHtml = newCamp.formatShow()
+      $('#app_container').append(campHtml)
     })
+  })
 
 
-  function Camp(camp) {
-    this.id = camp.id
-    this.name = camp.name
-    this.description = camp.description
-    this.location = camp.location
-  }
+  $('#new_camp_form').on("submit", function(event) {
+    event.preventDefault()
+    $('#app_container').html('')
+
+    const values = $(this).serialize()
+
+    $.post("/camps", values).done(function(data) {
+      const newCamp = new Camp(data)
+      const htmlToAdd = newCamp.formatShow()
+      $('#app_container').append(htmlToAdd)
+    })
+  })
 
 
-  Camp.prototype.formatIndex = function() {
-    let campsHtml = `
-      <h3 style="font-weight:500;">${this.location}</h3>
-      <p>
-      <strong>
-        <a href="/camps/${this.id}" data-id="${this.id}" class="show_link">${this.name}</a>
-      </strong>
-      <br>
-      <br>
-      ${this.description}
-      <br>
-      <br>
-      <br>
-      </p>
-    `
-    return campsHtml;
-  };
+function Camp(camp) {
+  this.id = camp.id
+  this.name = camp.name
+  this.description = camp.description
+  this.location = camp.location
+  this.activities = camp.activities
+}
 
 
-  Camp.prototype.formatShow = function() {
-    let campHtml = `
-      <h2>${this.name}</h2>
-      <br>
-      <div>
-      <h3>${this.location}</h3>
-      ${this.description}
-      </div>
-      <p style="text-align:center;">
-        <a href="/camps/${this.id}/activities" data-id="${this.id}" class="show_activities_link">View ${this.name} Activities</a>
-      </p>
+Camp.prototype.formatIndex = function() {
+  let campsHtml = `
+    <h3 style="font-weight:500;">${this.location}</h3>
+    <p>
+    <strong>
+      <a href="/camps/${this.id}" data-id="${this.id}" class="show_link">${this.name}</a>
+    </strong>
+    <br>
+    <br>
+    ${this.description}
+    <br>
+    <br>
+    <br>
+    </p>
+  `
+  return campsHtml;
+};
 
-    `
-    return campHtml;
-  };
+
+Camp.prototype.formatShow = function() {
+  let campActivities = this.activities
+
+  return (`
+    <h2>${this.name}</h2>
+    <br>
+    <div>
+    <h3>${this.location}</h3>
+    ${this.description}
+    </div>
+    <br>
+      ${
+        campActivities.map(activity => {
+          return `
+            ${activity.name}
+            <br>
+            ${activity.description}
+            <br>
+            <br>
+          `
+        }).join('')
+      }
+  `)
+};
